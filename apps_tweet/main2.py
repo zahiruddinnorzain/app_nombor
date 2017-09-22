5f92f0b5bc3412a9d88285e261243a370c61230d
zahiruddin <kepaknaga@gmail.com>
2017-09-22 20:39:46 +0800

    features try and except. dan command quit

--- apps_tweet/main2.py
+++ apps_tweet/main2.py
@@ -1,5 +1,6 @@
 import tweepy
 import os
+import sys
 
 def get_api(cfg):
     auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
@@ -7,7 +8,8 @@ def get_api(cfg):
     return tweepy.API(auth)
 
 def main():
-    now_status = "Welcome"
+    os.system('cls')
+    now_status = "Welcome\nType 'quit' to exit"
     while 1:
 
         print ('================')
@@ -17,20 +19,30 @@ def main():
         print ('\n')
 
         twit = raw_input('Giliran: ')   #input nombor giliran
-        if twit == 'quit' : break   # exit console
-        # Fill in the values noted in previous step here
-        cfg = {
-            "consumer_key"          : "99iFuCOTzNQb5oMpNfT9IVZAN",
-            "consumer_secret"       : "Rm7e8jOE06q0jGny3pUDP36Xafxb3njjhZCarb8gDqxd1a5ihb",
-            "access_token"          : "906176499320897536-mlic1L6oJAY815owEY5wBdTZ9bOmPpm",
-            "access_token_secret"   : "TeqFHKEGSVOxqj5MmFfACVM2ZlfRqq65c7LTiEM873Q2H",
-            }
 
-        api = get_api(cfg)
-        tweet = twit    #string to tweet number
-        status = api.update_status(status=tweet)
-        # yes, tweet is called 'status' rather confusing
-        now_status = ("OK " + twit)  # status for success post
+        if twit == 'quit' or twit == 'QUIT' or twit == 'exit' or twit == 'EXIT' or twit == "'quit'" :     # exit console
+            os.system('cls')
+            sys.exit()
+            break
+
+        try:    # try to post
+            # Fill in the values noted in previous step here
+            cfg = {
+                "consumer_key"          : "99iFuCOTzNQb5oMpNfT9IVZAN",
+                "consumer_secret"       : "Rm7e8jOE06q0jGny3pUDP36Xafxb3njjhZCarb8gDqxd1a5ihb",
+                "access_token"          : "906176499320897536-mlic1L6oJAY815owEY5wBdTZ9bOmPpm",
+                "access_token_secret"   : "TeqFHKEGSVOxqj5MmFfACVM2ZlfRqq65c7LTiEM873Q2H",
+                }
+
+            api = get_api(cfg)
+            tweet = twit    #string to tweet number
+            status = api.update_status(status=tweet)
+            # yes, tweet is called 'status' rather confusing
+
+            now_status = ("OK " + twit)  # status for success post
+
+        except Exception:   # Fail to post
+            now_status = ("ERROR " + twit)
 
         os.system('cls')    # clear screen for windows shell
 
