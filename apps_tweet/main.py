271d966626ad55d38f1ffdf105fc10485dea2548
zahiruddin <kepaknaga@gmail.com>
2017-09-22 15:26:10 +0800

    fix bug cgf to cfg

--- apps_tweet/main.py
+++ apps_tweet/main.py
@@ -1,6 +1,6 @@
 import tweepy
 
-def get_api(cgf):
+def get_api(cfg):
     auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
     auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
     return tweepy.API(auth)
