df6002595c15d78f26c45a507623705a806c9ab9
zahiruddin <kepaknaga@gmail.com>
2017-09-22 15:11:14 +0800

    init main py in the folder apps_tweet

---
+++ apps_tweet/main.py
@@ -0,0 +1,23 @@
+import tweepy
+
+def get_api(cgf):
+    auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
+    auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
+    return tweepy.API(auth)
+
+def main():
+    # Fill in the values noted in previous step here
+    cfg = {
+        "consumer_key"          : "VALUE",
+        "consumer_secret"       : "VALUE",
+        "access_token"          : "VALUE",
+        "access_token_secret"   : "VALUE",
+    }
+
+    api = get_api(cfg)
+    tweet = "tweet from pyhton - 1"
+    status = api.update_status(status=tweet)
+    # yes, tweet is called 'status' rather confusing
+
+if __name__ == "__main__":
+    main()
